26099ba599c436d3fd798e68dbf69249f53ddef4
lostreasen <124521556+lostreasen@users.noreply.github.com>
2023-03-01 20:58:11 +0100

    Add files via upload

---
+++ LandingPage/js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".slider-main-block", {
+  loop: true,
+
+  navigation: {
+    nextEl: ".slider-main-block__arrow.swiper-button-next",
+    prevEl: ".slider-main-block__arrow.swiper-button-prev",
+  },
+});
